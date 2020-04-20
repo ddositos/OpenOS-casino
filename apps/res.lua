fe074abb019a2bd57dcefff0fb15a6a664a2b78a
doritosxxx <georgyvav@ya.ru>
2020-04-20 16:05:05 +0400

    Update res.lua

--- apps/res.lua
+++ apps/res.lua
@@ -1,5 +1,5 @@
 local com = require("component")
 local gpu = com.gpu
 
-io.write(string.format("Текущее разрешение: %ix%i\n", table.unpack(gpu.getResolution)))
-io.write(string.format("Максимальное разрешение: %ix%i\n", table.unpack(gpu.maxResolution)))
+io.write(string.format("Текущее разрешение: %ix%i\n", gpu.getResolution()))
+io.write(string.format("Максимальное разрешение: %ix%i\n", gpu.maxResolution()))
