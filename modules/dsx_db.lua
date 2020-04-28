5885bf17ac7a34a9a55e049d186070d2e1a0cac5
doritosxxx <georgyvav@ya.ru>
2020-04-28 10:03:30 +0400

    Update dsx_db.lua

--- modules/dsx_db.lua
+++ modules/dsx_db.lua
@@ -28,6 +28,7 @@ function Database:new(token)
 		params.token = self.token
 		local data = ""
 		if use_modem then
+			io.write(type, " ", serialization.serialize(params), "\n")
 			modem.send(server, port, type, serialization.serialize(params))
 			_, _, _, _, _, data = event.pull("modem_message", _, _, server, port)
 			if data == "error" then
