6e39e59ab4788915bcdb5b9d1e2c35ae8bb41e44
doritosxxx <georgyvav@ya.ru>
2020-04-29 19:11:07 +0400

    Update dsx_turret.lua

--- modules/dsx_turret.lua
+++ modules/dsx_turret.lua
@@ -10,33 +10,21 @@ function Turret:new( id, x, y, z )
 		z = z
 	}
 
-	function obj:attack( _x, _y, _z )
-		local x = math.floor(_x) + self.x*-1
-		local y = math.floor(_y) + self.y*-1
-		local z = math.floor(_z) + self.z*-1
-	
-	
-		if z >= 0 then
-			deg = math.deg(math.atan(x/z))*-1+180
-		else
-			deg = math.deg(math.atan(x/z))*-1
-		end
-	
-		local dist=math.sqrt(x*x+z*z)
-		local degv=math.deg(math.atan(y/dist))
-		--[[
+	function obj:attack( x, y, z )
 		local target = {
 			x = x - self.x,
 			y = y - self.y+1,
 			z = z - self.z
 		}
 		
-		local yaw = -math.deg(math.atan2(-target.x,-target.z))
+		local yaw = math.deg(math.atan2(target.x,-target.z))
+		if yaw < 0 then
+			yaw = yaw + 360
+		end
 		local dist = math.sqrt(target.x*target.x+target.z*target.z)
 		local pitch = math.deg(math.atan(target.y/dist))
 		pitch = math.min(pitch, 90)
 		pitch = math.max(pitch, -45)
-		]]--
 		self.proxy.moveTo( dist, degv)
 		self.proxy.fire()
 	end
